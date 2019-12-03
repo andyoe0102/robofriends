6beaa841f33db108bd2abd4bf7b21ee1c89445c2
Andy-Oe <oe.andy0102@gmail.com>
2019-12-02 22:42:18 -0600

    Updates

---
+++ precache-manifest.bf0d3b6aac1ddd51b2bde51d469e0529.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d5f6e94c1ea61acd6858927cfa110d0",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "57520ebe50937ca2be6c",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "58cb595ee54c350ff726",
+    "url": "/robofriends/static/css/main.6ab5020e.chunk.css"
+  },
+  {
+    "revision": "57520ebe50937ca2be6c",
+    "url": "/robofriends/static/js/2.db79ab96.chunk.js"
+  },
+  {
+    "revision": "58cb595ee54c350ff726",
+    "url": "/robofriends/static/js/main.f2724fbc.chunk.js"
+  },
+  {
+    "revision": "5645dfe01f57d087ff9e",
+    "url": "/robofriends/static/js/runtime-main.47a7d18e.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/robofriends/static/media/SEGA.82e09f78.woff"
+  }
+]);
